fcc1ba5e0d85687fe7755f610539524c2dde662e
Patrick Rivera <patrick.rivera@instacart.com>
2018-12-01 11:23:09 -0800

    Finished polls app

--- active-record/polls_app/app/models/user.rb
+++ active-record/polls_app/app/models/user.rb
@@ -10,4 +10,21 @@ class User < ApplicationRecord
     primary_key: :id,
     foreign_key: :respondent_id,
     class_name: 'Response'
+
+  def completed_polls
+    polls = Poll.find_by_sql([<<-SQL, id])
+      SELECT polls.id, polls.title
+      FROM polls
+        INNER JOIN questions
+          ON questions.poll_id = polls.id
+        INNER JOIN answer_choices
+          ON answer_choices.question_id = questions.id
+        INNER JOIN responses
+          ON responses.answer_choice_id = answer_choices.id
+      WHERE
+        responses.respondent_id = ?
+    SQL
+
+    polls
+  end
 end
